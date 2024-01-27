7d2408ad938707a30e61d5e54f5039ff7cec475d
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-26 23:34:24 -0800

    Added globals.css, trying to get theme to be more blue

--- space-journal/src/app/page.tsx
+++ space-journal/src/app/page.tsx
@@ -11,14 +11,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
         <Alert>
             <AlertTitle>Heads up!</AlertTitle>
             <AlertDescription>

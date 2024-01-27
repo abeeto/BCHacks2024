cce6be145cef6f95dd978f0639d93034cafea579
KaelPearson <kaelpearson@gmail.com>
2024-01-27 02:19:55 -0800

    Changed to disable on enter for name input

--- space-journal/src/app/hello.tsx
+++ space-journal/src/app/hello.tsx
@@ -6,14 +6,25 @@ const WelcomePage = () => {
     const [name, setName] = useState('');
     const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
         setName(e.target.value);
-        console.log(name);
         localStorage.setItem('name', e.target.value.toString());
       };
+      function isEnterKeyPressed(event: KeyboardEvent): boolean {
+        return event.key === "Enter";
+      }
+      
+      // Example usage:
+      document.addEventListener("keydown", (event: KeyboardEvent) => {
+        if (isEnterKeyPressed(event)) {
+          (document.getElementById("nameInput") as HTMLInputElement).disabled = true;
+        }
+      });
+
+      
   return (
     <div className="h-screen flex row items-center ">
       <div className="text-4xl pr-2">Welcome,</div>
-      <Input className="border-0 border-b-2 border-blue-300 focus:none text-blue-300 text-4xl focus-visible:ring-0 "
-      placeholder="sexy" onChange={handleInputChange} />
+      <Input id="nameInput" className="border-0 border-b-2 border-blue-300 focus:none text-blue-300 text-4xl focus-visible:ring-0 "
+      placeholder="sexy" onChange={handleInputChange}  />
     </div>
     
   )

ea03c8b3fb8ae281a6082a31cb92b03d0b06c0ed
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-27 01:13:10 -0800

    This should work now.

--- space-journal/src/app/hello.tsx
+++ space-journal/src/app/hello.tsx
@@ -9,16 +9,10 @@ const WelcomePage = () => {
         console.log(name);
       };
   return (
-<<<<<<< HEAD
     <div className="h-screen flex row items-center ">
       <div className="text-4xl pr-2">Welcome,</div>
       <Input className="border-0 border-b-2 border-blue-300 focus:none text-blue-300 text-4xl focus-visible:ring-0 "
       placeholder="sexy" onChange={handleInputChange} />
-=======
-    <div className="welcome-container">
-      <h1 className="welcome-text">Welcome,</h1>
-      <Input onChange={handleInputChange} placeholder="sexy "/>
->>>>>>> 073e3c61a3f77934ebfff72a9c6a3521734433cc
     </div>
     
   )

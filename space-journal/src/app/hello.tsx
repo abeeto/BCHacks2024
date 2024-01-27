5ca418dc23f4eeb33a6bb5daea9c60bd363a05ec
KaelPearson <kaelpearson@gmail.com>
2024-01-27 02:55:06 -0800

    animation change

--- space-journal/src/app/hello.tsx
+++ space-journal/src/app/hello.tsx
@@ -7,7 +7,6 @@ const WelcomePage = () => {
     const [name, setName] = useState('');
     const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
         setName(e.target.value);
-        console.log(name);
         localStorage.setItem('name', e.target.value.toString());
       };
       function isEnterKeyPressed(event: KeyboardEvent): boolean {
@@ -16,9 +15,14 @@ const WelcomePage = () => {
       
       // Example usage:
       document.addEventListener("keydown", (event: KeyboardEvent) => {
-        if (isEnterKeyPressed(event)) {
-          (document.getElementById("nameInput") as HTMLInputElement).disabled = true;
+        let nameInput = document.getElementById("nameInput");
+        if (isEnterKeyPressed(event) && event.target === nameInput) {
+          (nameInput as HTMLInputElement).disabled = false;
           jumpToSection("hobby")
+          if(nameInput != null){
+            nameInput.className = "flex h-9 w-full rounded-md bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 border-0 focus:none text-blue-300 text-4xl focus-visible:ring-0";
+            nameInput?.blur();
+          }
         }
       });
 

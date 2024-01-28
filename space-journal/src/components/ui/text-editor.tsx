503377ddaee1f7c38ae2878ede500fcf1570e850
Adam Fipke <adamfipke@gmail.com>
2024-01-27 21:00:21 -0800

    border changes with sentiment value

--- space-journal/src/components/ui/text-editor.tsx
+++ space-journal/src/components/ui/text-editor.tsx
@@ -1,18 +1,52 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "../../app/styles/textbox.css";
 import { getJournalAtDate, getJournalEntries, setJournal } from '../../Helpers/Helper'
-var sentiment = require( 'wink-sentiment' );
+var sentiment = require('wink-sentiment');
 
 // THIS WHOLE PART LOADS THE PREV JOURNAL
 var prevJournal = "";
 const storedJournalEntries = getJournalEntries();
 const date = new Date();
 const str = "" + date.getFullYear() + date.getMonth() + date.getDate();
-if(storedJournalEntries.includes(str)){
-    prevJournal = JSON.parse(getJournalAtDate(str)??"{}").text;
+if (storedJournalEntries.includes(str)) {
+    prevJournal = JSON.parse(getJournalAtDate(str) ?? "{}").text;
 }
+
+// for converting rgb to hex
+function componentToHex(c : number) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+const rgbToHex = (r : number, g : number, b : number) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+// Function to get border color based on data value
+const convertSentimentToColor = (value: number) => {
+    return rgbToHex(125 - (value * 10), 125 + (value * 10), 0);
+}
+
+
+function runthispls(content: string) {
+    // THIS REMOVES ALL HTML TAGS
+    const adjustedContent = content.replace(/(<([^>]+)>)/ig, '');
+    // Get Sentiment score
+    const sentimentValue = sentiment(adjustedContent).score;
+
+    const textbox = document.getElementsByClassName("ql-container")[0] as HTMLDivElement;
+    const toolbar = document.getElementsByClassName("ql-toolbar")[0] as HTMLDivElement;
+    
+    //remove old border colors (can be generated)
+    
+    // change the css of the color-sentiment class based on sentiment value
+    textbox.style.borderColor = convertSentimentToColor(sentimentValue);
+    toolbar.style.borderColor = convertSentimentToColor(sentimentValue);
+    
+}
+
+
 
 export default function TextEditor() {
     const [value, setValue] = useState(prevJournal);
@@ -42,6 +76,11 @@ export default function TextEditor() {
             ],
         };
     }, []);
+
+    // Run your function whenever the text changes
+    useEffect(() => {
+        runthispls(value);
+    }, [value]);
 
     return (
         <div>

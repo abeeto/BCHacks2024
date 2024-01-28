61163ebb0ca095a82a4e4d958099c0bccbb8caec
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-27 19:38:00 -0800

    Hobbies page done!!

--- space-journal/src/app/hobby.tsx
+++ space-journal/src/app/hobby.tsx
@@ -2,30 +2,38 @@
 import React, { useState, useEffect } from 'react';
 import { NewHobbyDrawer } from '@/components/ui/newHobby';
 import StreakCounter from '@/components/ui/streakCounter';
+import HobbyText from '@/components/ui/HobbyText';
 import {getHobbies, saveHobbies} from '../Helpers/Helper'
 const HobbyTracker = () => {
   const [hobbies, setHobbies] = useState(getHobbies());
   const updateStreakCount = (index: number, toAdd: boolean) => {
     
-    const newHobbies = hobbies.map((hobby, i) => 
-      toAdd ? (i === index ? { ...hobby, count: hobby.count + 1 } : hobby):
-              (i === index ? { ...hobby, count: Math.max(hobby.count -1, 0)} : hobby)
-    );
+  const newHobbies = hobbies.map((hobby, i) => 
+    toAdd ? (i === index ? { ...hobby, count: hobby.count + 1 } : hobby):
+            (i === index ? { ...hobby, count: Math.max(hobby.count -1, 0)} : hobby)
+  );
+  setHobbies(newHobbies);
+  };
+  const deleteHobby = (index: number) => {
+    // Using filter to remove the item at the given index
+    const newHobbies = hobbies.filter((_, i) => i !== index);
     setHobbies(newHobbies);
   };
 
+  useEffect(() => {
+    saveHobbies(hobbies);
+  }, [hobbies]); // Save hobbies when they change
 
-  saveHobbies(hobbies)
  
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-between flex">
+    <div className="flex min-h-screen w-1/2 z-50 flex-col items-center justify-between flex">
       <div id="hobby" className="align-top"></div>
       <div className='flex flex-col w-4/5'> 
           <div className ="text-4xl mb-5">Have you been..</div>
             {hobbies.map((hobby, index) => (
-              <div key={index} className="flex flex-row">
-                <div className='text-xl mt-3 pl-4 pt-2 pb-2 bg-secondary rounded-sm flex-grow'>{hobby.name}</div>
+              <div key={index} className="flex flex-row ">
+                <HobbyText hobby={hobby} index={index} deleteHobby={deleteHobby}/>
                 <StreakCounter hobby={hobby} index={index} updateStreakCount={updateStreakCount}/>
               </div>
             ))}

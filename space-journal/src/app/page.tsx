21e40b37f5c83e67d5c6708f2b935dbd64227754
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-01-27 21:35:52 -0800

    Add ROCKET

--- space-journal/src/app/page.tsx
+++ space-journal/src/app/page.tsx
@@ -15,6 +15,7 @@ import MoodGrid from "@/components/ui/moodGrid";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import ScrollDownButton from "@/components/ui/ScrollDownButton";
 import { CardDemo } from "@/components/ui/card-demo";
+import Rocket from "@/../public/rocket.png"
 export default function Home() {
   const [nextSection, setNextSection] = useState('hobby');
   const handleScroll = () => {
@@ -41,6 +42,7 @@ export default function Home() {
     //flex min-h-screen flex-col items-center justify-between pb-24
     <main className="flex-col w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       {nextSection !== "last" ? <ScrollDownButton targetId={nextSection} /> : <div></div>}
+      <Image src={Rocket} width="100" height="100" alt="Description" className = "fixed bottom-0 right-0 shake" />
       <WelcomePage />
       <HobbyPage />
       <JournalPage />

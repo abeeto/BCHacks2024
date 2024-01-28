d698090bdfebd1ec047ebc7e583adf7c00093ed6
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-01-27 20:37:09 -0800

    add scroll file

---
+++ space-journal/src/components/ui/ScrollDownButton.tsx
@@ -0,0 +1,42 @@
+'use client'
+import React from 'react';
+import { FaArrowDown } from 'react-icons/fa'; // Make sure to import the icon
+
+const ScrollDownButton = ({ targetId }) => {
+  const scrollToTarget = () => {
+    const target = document.getElementById(targetId);
+    if (target) {
+      console.log(target.id)
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <button
+      onClick={scrollToTarget}
+      style={{
+        position: 'fixed',
+        bottom: '20px',
+        left: '50%', // Set the left edge to be in the center of the screen
+        transform: 'translateX(-50%)', // Shift button left by half its width to center it
+        width: '50px',
+        height: '50px',
+        borderRadius: '50%',
+        backgroundColor: '#007bff',
+        color: 'white',
+        border: 'none',
+        cursor: 'pointer',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 9999,
+      }}
+      aria-label="Scroll down"
+    >
+      <FaArrowDown />
+    </button>
+  );
+};
+
+export default ScrollDownButton;

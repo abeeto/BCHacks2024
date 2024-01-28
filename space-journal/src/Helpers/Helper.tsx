919658e84de8cba94d0b5e5f732f7e30ff4808c3
KaelPearson <kaelpearson@gmail.com>
2024-01-28 00:11:57 -0800

    again

--- space-journal/src/Helpers/Helper.tsx
+++ space-journal/src/Helpers/Helper.tsx
@@ -102,7 +102,7 @@ export function averageSentimentForPastYear() {
               // Ensure the entry has sentiment property
               if (entry && entry.sentiment !== undefined) {
                   totalSentiment += entry.sentiment;
-                  console.log(entry.sentiment)
+                  //console.log(entry.sentiment)
                   numberOfEntries++;
               }
           }
